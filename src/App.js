bd4e00c7d942ab4a27e5b3720add5fc842bd55b6
Samuel Ward <samuelleeward9@gmail.com>
2024-01-15 21:18:46 +0100

    upload form added

--- src/App.js
+++ src/App.js
@@ -17,6 +17,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>mpthree</h1>
+        <form action="/api/post" method="post" enctype="multipart/form-data">
+          <label for="file">file: </label>
+          <input id="file" name="mp3" type="file" /><br />
+          <label for="songName">song name: </label>
+          <input id="songName" name="songName" type="text" /><br />
+          <label for="artistName">artist name: </label>
+          <input id="artistName" name="artistName" type="text" /><br />
+          <label for="duration">duration (delete this later): </label>
+          <input id="duration" name="duration" type="text" /><br />
+          <input type="submit" value="Upload" />
+        </form>
+        <h2>Uploaded mpthrees</h2>
         {audios.map(audio => {
           return <div>
             <h3>{audio.songName} - {audio.artistName}</h3>
