6fa6d170cdd6aea8999e9576284c8b676e4bb40e
Samuel Ward <samuelleeward9@gmail.com>
2024-02-15 20:21:21 +0100

    allowed formats simplified

--- src/components/NewAudioForm.js
+++ src/components/NewAudioForm.js
@@ -4,7 +4,7 @@ export function NewAudioForm({ userId }) {
             <h2>upload</h2>
             <form className="form" action="/api/post" method="post" encType="multipart/form-data">
                 <input id="mp3" name="mp3" type="file" accept=".mp3" required/>
-                <input id="image" name="image" type="file" accept="image/png, image/jpeg"/>
+                <input id="image" name="image" type="file" accept="image/jpeg"/>
                 <input id="songName" name="songName" type="text" placeholder="Song Name" required/>
                 <input id="artistName" name="artistName" type="text" placeholder="Artist Name" required/>
                 <input id="uploaderId" name="uploaderId" type="hidden" value={userId} required />
