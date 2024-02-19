30d75aa1b403976a1bd3bfd8d66f66c9a46df1a6
Samuel Ward <samuelleeward9@gmail.com>
2024-02-19 18:05:10 +0100

    custom file select fields

--- src/components/NewAudioForm.js
+++ src/components/NewAudioForm.js
@@ -1,10 +1,35 @@
+import { useState } from "react"
+
 export function NewAudioForm({ userId }) {
+    const [mp3, setMp3] = useState('No current file')
+    const [image, setImage] = useState('No current image')
+
+    function handleMp3Change(e) {
+        if (e.target.files[0] !== undefined) {
+            setMp3(e.target.files[0].name)
+        }
+    }
+
+    function handleImageChange(e) {
+        if (e.target.files[0] !== undefined) {
+            setImage(e.target.files[0].name)
+        }
+    }
+
     return (
         <div>
             <h2>upload</h2>
             <form className="form" action="/api/post" method="post" encType="multipart/form-data">
-                <input id="mp3" name="mp3" type="file" accept=".mp3" required/>
-                <input id="image" name="image" type="file" accept="image/jpeg"/>
+                <div className="input-wrap">
+                    <label id="mp3-label" htmlFor="mp3" className="btn">Choose mp3 file</label>
+                    <p>{mp3}</p>
+                    <input className="file-input" id="mp3" name="mp3" type="file" accept=".mp3" onChange={handleMp3Change} required />
+                </div>
+                <div className="input-wrap">
+                    <label id="image-label" htmlFor="image" className="btn">Choose cover image</label>
+                    <p>{image}</p>
+                    <input className="file-input" id="image" name="image" type="file" onChange={handleImageChange} accept="image/jpeg"/>
+                </div>
                 <input id="songName" name="songName" type="text" placeholder="Song Name" required/>
                 <input id="artistName" name="artistName" type="text" placeholder="Artist Name" required/>
                 <input id="uploaderId" name="uploaderId" type="hidden" value={userId} required />
